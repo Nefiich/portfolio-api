f323d3ca043ed8e69fe6b426724f73c821395db3
Emin Nefic <emin.nefic@gmail.com>
2021-10-11 22:56:07 +0200

    Fixing ip

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
 
 app.use(express.json());
 
-function isFromServer(req, res, next){
+/*function isFromServer(req, res, next){
     if(req.ip === '141.170.195.102') return next();
     else{
         var err = new Error('Not Found, your ip : ' + req.ip);
         err.status = 404;
         return next(err);
     }
-}
+}*/
 
 const coursesRoute = require("./routes/courses");
 app.use("/api/courses", coursesRoute);
