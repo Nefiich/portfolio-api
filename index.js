6fb9309e18174a1395eaa080438dd305f5b7c9ed
Emin Nefic <emin.nefic@gmail.com>
2021-10-11 22:41:48 +0200

    Changed start command

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const app = express();
 
 app.use(express.json());
 
+function isFromServer(req, res, next){
+    if(req.ip === '127.0.0.1') return next();
+    else{
+        var err = new Error('Not Found');
+        err.status = 404;
+        return next(err);
+    }
+}
+
 const coursesRoute = require("./routes/courses");
 app.use("/api/courses", coursesRoute);
 
