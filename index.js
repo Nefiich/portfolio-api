932c8d66ce0265a299c11d5948e10111f6b2a147
Emin Nefic <emin.nefic@gmail.com>
2021-10-11 22:48:26 +0200

    Changed fixing port

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ app.use("/api/projects", projectsRoute);
 
 
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
